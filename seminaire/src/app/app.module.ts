27d52e33a54021533f0276ace252fae6cdc5c679
romain Hassouni <romain.hassouni@etu.univ-amu.fr>
2024-04-01 21:37:46 +0200

    version formulaire 2

--- seminaire/src/app/app.module.ts
+++ seminaire/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule} from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -9,7 +9,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule ,HttpClientModule
   ]
 
 })
